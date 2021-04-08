3dd96c58c18d5e93ee1739c2e76f134e3f0231ce
saffaalvi <alvi118@uwindsor.ca>
2021-04-07 21:18:20 -0400

    feat(jupyter): add translations

--- components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-rok/form-rok.component.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-rok/form-rok.component.ts
@@ -15,6 +15,7 @@ import { Router } from '@angular/router';
 import { getFormDefaults, initFormControls } from '../form-default/utils';
 import { JWABackendService } from 'src/app/services/backend.service';
 import { FormDefaultComponent } from '../form-default/form-default.component';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-form-rok',
@@ -34,8 +35,9 @@ export class FormRokComponent extends FormDefaultComponent
     public router: Router,
     public popup: SnackBarService,
     public rok: RokService,
+    public translate: TranslateService
   ) {
-    super(ns, backend, router, popup);
+    super(ns, backend, router, popup, translate);
   }
 
   ngOnInit() {
